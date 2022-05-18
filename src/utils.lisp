04f5e3e0582d1ed9712b58716d68b9a21545c953
Walpurgisnatch <Kawemirs@gmail.com>
2022-05-18 17:08:07 +0700

    preparations works

--- src/utils.lisp
+++ src/utils.lisp
@@ -4,5 +4,15 @@
 
 (in-package :wordle.utils)
 
-(defun find-char (char string)
-  (find (coerce char 'character) string :test #'char-equal))
+(defun find-char (symb word)
+  (find (to-char symb) word :test #'char-equal))
+
+(defun duplicate (symb word)
+  (let ((first (pos symb word)))
+    (when first (pos symb word (1+ first)))))
+
+(defun pos (symb word &optional (start 0))
+  (position (to-char symb) word :test #'char-equal :start start))
+
+(defun to-char (char)
+  (coerce char 'character))
--- src/wordle.lisp
+++ src/wordle.lisp
@@ -6,12 +6,28 @@
 (in-package :wordle)
 
 (defparameter *dictionary* '("ооооо" "салат" "лыжня" "зерги" "ясбью" "номер" "вагон" "комар"))
+(defparameter *alphabet* '(("а" . 2172) ("о" . 1476) ("к" . 1237) ("р" . 1100) ("и" . 935)
+ ("т" . 913) ("е" . 891) ("л" . 837) ("н" . 810) ("с" . 792) ("у" . 644)
+ ("п" . 588) ("м" . 546) ("в" . 470) ("б" . 462) ("д" . 445) ("з" . 357)
+ ("г" . 335) ("я" . 275) ("ь" . 271) ("ш" . 263) ("ч" . 213) ("х" . 199)
+ ("ы" . 197) ("ж" . 179) ("ф" . 173) ("ц" . 156) ("й" . 152) ("ё" . 151)
+ ("ю" . 90) ("щ" . 45) ("э" . 35) ("ъ" . 5)))
 (defparameter *popular* '("о" "у" "а" "и" "н" "т" "с" "р" "в" "л"))
 
 (defun first-word ()
-  (loop for i in *dictionary* do
-    (when (find-char (elt *popular* 0) i)
-        (return i))))
+  (let ((words nil))
+    (loop for word in *dictionary* collect
+      (cons word (check-word word)))))
+
+(defun check-word (word)
+  (let ((rate 0))
+    (loop for i in *popular* do
+      (cond ((and (find-char i word)
+                  (not (duplicate i word)))
+             (incf rate))
+            ((duplicate i word)
+             (decf rate))))
+    rate))
 
 (defun random-word ()
   (apply #'concatenate 'string
